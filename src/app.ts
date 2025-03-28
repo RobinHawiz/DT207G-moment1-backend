03052c97be6c5fa831682fe49a023b72b084f381
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2025-03-28 20:55:19 +0100

    ✨ feat: integrate coursesRoutes into app initialization for routing
    Update app.ts to include coursesRoutes, enabling API requests routing through the '/courses' path with a shared ConnectionPool.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import "./config/env.js";
 import express, { Express } from "express";
 import { ConnectionPool } from "mssql";
 import { connectToDatabase } from "./config/db.js";
+import { coursesRoutes } from "./routes/coursesRoutes.js";
 
 export async function createApp(): Promise<express.Express> {
   const app: Express = express();
   // Connect to db
   const pool: ConnectionPool = await connectToDatabase();
+  const routes = coursesRoutes(pool);
+
+  app.use("/courses", routes);
 
   return app;
 }
