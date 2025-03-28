839a8879d2e841cc99ba7d725c75d6109e0c3ce3
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2025-03-28 11:02:29 +0100

    📝 docs: add comments to clarify dotenv configuration and server startup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 
+// Load environment variables
 dotenv.config();
 
 const APP_PORT: number = Number(process.env.APP_PORT) || 3000;
 
+// Start server
 app.listen(APP_PORT, (): void => {
   console.log(`Server is running on port ${APP_PORT}`);
 });
