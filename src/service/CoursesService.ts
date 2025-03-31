cfad86d689a323f6ab1df943b9da4d32a84db882
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2025-03-31 11:52:31 +0200

    🛡️ refactor(types): add explicit Promise return types to CoursesService methods

--- src/service/CoursesService.ts
+++ src/service/CoursesService.ts
@@ -1,6 +1,7 @@
 import { ConnectionPool } from "mssql";
 import * as dataAccess from "../dataAccess/coursesDataAccess.js";
 import { Request } from "express";
+import { ICourses } from "../models/ICourses.js";
 
 export class CoursesService {
   private readonly pool: ConnectionPool;
@@ -9,15 +10,15 @@ export class CoursesService {
     this.pool = pool;
   }
 
-  async getAllCourses() {
+  async getAllCourses(): Promise<ICourses[]> {
     return dataAccess.getAllCourses(this.pool);
   }
 
-  async insertCourses(req: Request) {
+  async insertCourses(req: Request): Promise<void> {
     return dataAccess.insertCourses(this.pool, req);
   }
 
-  async deleteCourses(req: Request) {
+  async deleteCourses(req: Request): Promise<void> {
     const courseExists: number = await dataAccess.checkCourses(this.pool, req);
     if (!courseExists) {
       throw new Error("Kursen med det Id't existerar inte!");
